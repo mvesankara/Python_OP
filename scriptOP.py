49af43b6e6d6c10ef10356a1fa6869908f40ff0c
Sankara MVE <mvesankara@yahoo.fr>
2022-02-14 00:01:29 +0100

    création de fichier csv, sans recuperation des données

--- scriptOP.py
+++ scriptOP.py
@@ -1,3 +1,4 @@
+import csv
 import requests
 from bs4 import BeautifulSoup
 url = "https://www.it-connect.fr/actualites/"
@@ -5,8 +6,20 @@ reponse = requests.get(url)
 page = reponse.content
 soup = BeautifulSoup(page, 'html.parser')
 
-titres = soup.find_all("h2")
-#print(titres)
+titres = soup.find_all("h2", class_="entry-title")
+titres_textes = []
+for titre in titres:
+    titres_textes.append(titre.string)
+
 
 descriptions = soup.find_all("div", class_="entry-content clearfix")
-print(titres, descriptions)
+descriptions_textes = []
+for description in descriptions:
+    descriptions_textes.append(description.string)
+
+en_tete = ['titre', 'description']
+with open('data.csv', 'w')  as csv_file:
+    writer = csv.writer(csv_file, delimiter=',')
+    writer.writerow(en_tete)
+    for titre, description in zip(titres_textes, descriptions_textes):
+        writer.writerow([titre, description])
