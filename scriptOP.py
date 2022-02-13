200f92ff112b2c33bcd9bc732fd796fb12ab8b68
Sankara MVE <mvesankara@yahoo.fr>
2022-02-13 23:27:49 +0100

    Recuperation titres et descriptions

--- scriptOP.py
+++ scriptOP.py
@@ -6,4 +6,7 @@ page = reponse.content
 soup = BeautifulSoup(page, 'html.parser')
 
 titres = soup.find_all("h2")
-print(titres)
+#print(titres)
+
+descriptions = soup.find_all("div", class_="entry-content clearfix")
+print(titres, descriptions)
